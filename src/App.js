9a15e0ac92e99accf0e345d0240f155f83f9250b
Vishnu V <vishnukpn884@gmail.com>
2021-03-21 23:10:21 +0530

    filterhandler error required

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
-// importing Componets
+// importing Components
 import Form from './Components/Form';
-import TodoList from './Components/TodoList'
+import TodoList from './Components/TodoList';
 
 
 
 function App() {
+  // declare states
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const { filteredTodos, setFilteredTodos } = useState([]);
+
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+
+  //functions
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+
+    }
+  };
   return (
     <div className="App">
-       <h1>hello react</h1>
-       <Form />
-       <TodoList />
+      <h1>hello react {inputText}</h1>
+      <Form
+        setStatus={setStatus} status={status}
+        inputText={inputText} todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText} />
+      <TodoList
+        setTodos={setTodos}
+        todos={todos} />
     </div>
   );
-}
+};
 
 export default App;
--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,9 +1,23 @@
 import React from 'react';
+// import Components
+import Todo from './Todo';
 
-const TodoList = () => {
-    return(
+
+const TodoList = ({ todos, setTodos }) => {
+
+    return (
         <div className="todo-container">
-            <ul className="todo-list"></ul>
+            <ul className="todo-list">
+                {todos.map((todo) => (
+                    <Todo
+                        setTodos={setTodos}
+                        todos={todos}
+                        key={todo.id}
+                        text={todo.text}
+                        todo={todo} />
+                ))}
+            </ul>
+
         </div>
     );
 };
